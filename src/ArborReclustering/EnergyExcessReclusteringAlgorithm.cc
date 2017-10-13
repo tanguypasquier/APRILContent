94e54c915e5de5f4cbbac7d4e93957fbfe965fa0
Bo Li <bo.li@llr.in2p3.fr>
2017-10-13 10:59:17 +0200

    Modified EnergyExcessReclustering

--- src/ArborReclustering/EnergyExcessReclusteringAlgorithm.cc
+++ src/ArborReclustering/EnergyExcessReclusteringAlgorithm.cc
@@ -74,10 +74,16 @@ namespace arbor_content
       // check for chi2, energy excess and asymmetric cluster
       if( (chi*chi < m_minChi2ToRunReclustering || chi < 0.f)  || ((*trackList.begin())->GetEnergyAtDca() < m_minTrackMomentum))
         continue;
-
+#if 0
+      // I don't understand the reason we have this statement, and it will lose energy.
+      // Let us comment out it currently.
       // check for clusters that leave the detector
       if(ClusterHelper::IsClusterLeavingDetector(this->GetPandora(), pCluster))
+      {
+        std::cout << "*********** Cluster leaving detector ..." << std::endl; 
         continue;
+      }
+#endif
 
       // prepare clusters and tracks for reclustering
       pandora::ClusterList reclusterClusterList;
