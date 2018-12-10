f1a1fab73b24e47f12a55e933072498e3f704712
libo929 <libo0929@gmail.com>
2018-12-10 16:03:26 +0100

    Added connector property tree in CheatingClusterCleaning

--- src/ArborCheating/CheatingClusterCleaningAlgorithm.cc
+++ src/ArborCheating/CheatingClusterCleaningAlgorithm.cc
@@ -9,6 +9,8 @@
 #include "Pandora/AlgorithmHeaders.h"
 
 #include "ArborCheating/CheatingClusterCleaningAlgorithm.h"
+#include "ArborObjects/CaloHit.h"
+#include "ArborHelpers/HistogramHelper.h"
 
 using namespace pandora;
 
@@ -29,20 +31,62 @@ StatusCode CheatingClusterCleaningAlgorithm::Run()
             const MCParticle *const pMainMCParticle(MCParticleHelper::GetMainMCParticle(pCluster));
 
             // Remove all calo hits that do not correspond to the cluster main mc particle
-            OrderedCaloHitList orderedCaloHitList(pCluster->GetOrderedCaloHitList());
+			pandora::OrderedCaloHitList orderedCaloHitList(pCluster->GetOrderedCaloHitList());
 
-            for (OrderedCaloHitList::const_iterator itLyr = orderedCaloHitList.begin(), itLyrEnd = orderedCaloHitList.end(); itLyr != itLyrEnd; ++itLyr)
+            for (pandora::OrderedCaloHitList::const_iterator itLyr = orderedCaloHitList.begin(), itLyrEnd = orderedCaloHitList.end(); itLyr != itLyrEnd; ++itLyr)
             {
-                for (CaloHitList::const_iterator hitIter = itLyr->second->begin(), hitIterEnd = itLyr->second->end(); hitIter != hitIterEnd; ++hitIter)
+                for (pandora::CaloHitList::const_iterator hitIter = itLyr->second->begin(), hitIterEnd = itLyr->second->end(); hitIter != hitIterEnd; ++hitIter)
                 {
                     try
                     {
-                        const CaloHit *const pCaloHit = *hitIter;
+                        const pandora::CaloHit *const pCaloHit = *hitIter;
                         const MCParticle *const pMCParticle(MCParticleHelper::GetMainMCParticle(pCaloHit));
 
                         if (pMainMCParticle != pMCParticle)
                         {
                             PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::RemoveFromCluster(*this, pCluster, pCaloHit));
+        
+							const arbor_content::CaloHit *const pArborCaloHit = dynamic_cast<const arbor_content::CaloHit *const>( pCaloHit );
+
+                            if(NULL == pArborCaloHit) continue;
+
+							//pArborCaloHit->GetDensity();
+							float fwdConnection = 0.;
+							float bakConnection = 0.;
+
+							const ConnectorList &fwdConnectorList(ArborContentApi::GetConnectorList(pArborCaloHit, FORWARD_DIRECTION));
+							const ConnectorList &bakConnectorList(ArborContentApi::GetConnectorList(pArborCaloHit, BACKWARD_DIRECTION));
+
+							for(auto fwdCon : fwdConnectorList)
+							{
+								float fwdConnectionLength = fwdCon->GetLength();
+
+								if( fwdConnectionLength > fwdConnection ) fwdConnection = fwdConnectionLength;
+							}
+
+							for(auto bakCon : bakConnectorList)
+							{
+								float bakConnectionLength = bakCon->GetLength();
+
+								if( bakConnectionLength > bakConnection ) bakConnection = bakConnectionLength;
+							}
+
+							float clusterEnergy = pCluster->GetHadronicEnergy();
+							float hitEnergy = pArborCaloHit->GetInputEnergy();
+			   
+							float hitCharge = pandora::PdgTable::GetParticleCharge( pMCParticle->GetParticleId() );
+							float cluCharge = pandora::PdgTable::GetParticleCharge( pMainMCParticle->GetParticleId() );
+
+							std::vector<float> vars;
+							vars.push_back( clusterEnergy );
+							vars.push_back( hitEnergy );
+							vars.push_back( fwdConnection );
+							vars.push_back( bakConnection );
+							vars.push_back( hitCharge );
+							vars.push_back( cluCharge );
+
+							extern HistogramManager AHM;
+		                    AHM.CreateFill("CheatingClusterCleaning", "clusterEnergy:hitEnergy:fwdConnection:bakConnection:hitCharge:cluCharge", vars);
                         }
                     }
                     catch (StatusCodeException &)
@@ -51,6 +95,7 @@ StatusCode CheatingClusterCleaningAlgorithm::Run()
                 }
             }
 
+#if 0
             // Repeat for isolated hits
             CaloHitList isolatedCaloHitList(pCluster->GetIsolatedCaloHitList());
 
@@ -71,6 +116,7 @@ StatusCode CheatingClusterCleaningAlgorithm::Run()
                 {
                 }
             }
+#endif
         }
         catch (StatusCodeException &)
         {
